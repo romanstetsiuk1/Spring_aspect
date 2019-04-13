d207aafbfc71c9c252a34e0dd98fc71215be7dd5
Roman Stetsiuk <romansteciuk021@gmail.com>
2019-04-13 10:25:27 +0200

    Add adnotations @AfterReturning @Around

--- src/main/java/com/romanstetsiuk/spring_aspect/UserAspect.java
+++ src/main/java/com/romanstetsiuk/spring_aspect/UserAspect.java
@@ -1,8 +1,8 @@
 package com.romanstetsiuk.spring_aspect;
 
-import org.aspectj.lang.annotation.Aspect;
-import org.aspectj.lang.annotation.Before;
-import org.aspectj.lang.annotation.Pointcut;
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.*;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
@@ -16,6 +16,27 @@ public class UserAspect {
     @Before("upperPointcut()")
     public void beforeUpper() {
         log.info("Przed metoda upper");
+    }
+
+    @AfterReturning(pointcut = "upperPointcut()", returning = "result")
+    public void returningName(JoinPoint joinPoint, String result) {
+        log.info("Zwrocono: " + result + " dla metody " + joinPoint.toLongString());
+    }
+
+    @Around("upperPointcut()")
+    public Object aroundName(ProceedingJoinPoint point) {
+        log.info("Przed");
+
+        Object object = null;
+
+        try {
+            point.proceed();
+        } catch (Throwable throwable) {
+            throwable.printStackTrace();
+        } finally {
+            log.info("po metodzie");
+            return object;
+        }
     }
 
     @Pointcut("execution(public String getUpperName())")
