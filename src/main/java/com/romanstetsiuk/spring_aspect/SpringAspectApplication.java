137c3c1ca8a66b754b47569725983f2ba2308165
Roman Stetsiuk <romansteciuk021@gmail.com>
2019-04-13 09:31:20 +0200

    Create project

---
+++ src/main/java/com/romanstetsiuk/spring_aspect/SpringAspectApplication.java
@@ -0,0 +1,13 @@
+package com.romanstetsiuk.spring_aspect;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAspectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringAspectApplication.class, args);
+    }
+
+}
